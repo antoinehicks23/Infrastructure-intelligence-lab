397de8f34ec2aa6b4a7ee6929bc62257a74a3aab
antoinehicks23 <antoinehicks23@gmail.com>
2026-02-24 03:26:15 +0400

    More string practice with indexes

--- Mosh_python_journey/strings_and_formats.py
+++ Mosh_python_journey/strings_and_formats.py
@@ -2,3 +2,13 @@ first_name = 'Antoine'
 last_name = 'Hicks'
 message = first_name + ' [' + last_name + '] is a coder'
 print(message)
+
+course = "Python's for Beginners"
+print(course)
+
+course = 'Python for Beginners'
+print(course)
+
+course = 'Python for Beginners'
+another = (course [:])
+print(another)
