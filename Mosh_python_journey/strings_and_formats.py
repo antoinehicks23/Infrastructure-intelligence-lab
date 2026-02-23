0df183f88cb9341f3509f8b866d809c486ce5155
antoinehicks23 <antoinehicks23@gmail.com>
2026-02-24 03:32:10 +0400

    msg is formatted version of message. Result = same

--- Mosh_python_journey/strings_and_formats.py
+++ Mosh_python_journey/strings_and_formats.py
@@ -1,7 +1,10 @@
 first_name = 'Antoine'
 last_name = 'Hicks'
 message = first_name + ' [' + last_name + '] is a coder'
+msg = f'{first_name} [{last_name}] is a coder'
+print(msg)
 print(message)
+
 
 course = "Python's for Beginners"
 print(course)
