872e821d23081b87fefffa2d643df81ab9d80c03
antoinehicks23 <antoinehicks23@gmail.com>
2026-02-24 02:35:29 +0400

    Restructuring Mosh python folder

--- Maintenance_Intelligence/Mosh python journey/Functions_basics.py
+++
@@ -1,4 +0,0 @@
-name = input('What is your name? ')
-favorite_color = input('What is your favorite color? ')
-print(name + ' likes ' + favorite_color)
-
--- Maintenance_Intelligence/Mosh python journey/Type_conversion.py
+++
@@ -1,3 +0,0 @@
-weight_lbs = float(input('weight_lbs: '))
-weight_kgs = weight_lbs * 0.45
-print(weight_kgs)
