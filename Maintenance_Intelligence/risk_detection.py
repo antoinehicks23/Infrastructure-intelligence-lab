354e98307ade2f7b1610156b657a237dd29dbec0
antoinehicks23 <antoinehicks23@gmail.com>
2026-04-17 14:26:52 +0400

    Original logic did return anything below 15 as high risk. Fault in logic. 10 is a big variance

--- Maintenance_Intelligence/risk_detection.py
+++ Maintenance_Intelligence/risk_detection.py
@@ -1,7 +1,7 @@
 def detect_temp_risk(temp_list):
     trend = temp_list[-1] - temp_list[0]
 
-    if trend >= 15:
+    if trend >= 10:
         return "High risk"
     elif trend >= 5:
         return "Medium risk"
