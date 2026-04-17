90070794a1a9dd4b0a1a91eb0555901830a295e5
antoinehicks23 <antoinehicks23@gmail.com>
2026-04-17 22:25:56 +0400

    Switched code to detect pressure

---
+++ Maintenance_Intelligence/pressure.py
@@ -0,0 +1,9 @@
+def detect_pressure_risk(pressure_list):
+    trend = pressure_list[-1] - pressure_list[0]
+
+    if trend <= -10:
+        return "High risk"
+    elif trend <= -5:
+        return "Medium risk"
+    else:
+        return "Normal"
