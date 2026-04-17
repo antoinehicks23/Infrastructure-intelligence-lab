6d7ec2567f0f013bb7844558efc761b982faa4e5
antoinehicks23 <antoinehicks23@gmail.com>
2026-04-17 14:23:51 +0400

    Code decision logic

---
+++ Maintenance_Intelligence/risk_detection.py
@@ -0,0 +1,9 @@
+def detect_temp_risk(temp_list):
+    trend = temp_list[-1] - temp_list[0]
+
+    if trend >= 15:
+        return "High risk"
+    elif trend >= 5:
+        return "Medium risk"
+    else:
+        return "Normal"
